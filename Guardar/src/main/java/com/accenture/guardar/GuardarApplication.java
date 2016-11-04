4aa85180de4bd96f94d6d8cf55568bd4587603de
josep.marco.selma <josep.marco.selma@192.168.3.97>
2016-11-04 14:25:19 +0100

    Microservicio

---
+++ Guardar/src/main/java/com/accenture/guardar/GuardarApplication.java
@@ -0,0 +1,12 @@
+package com.accenture.guardar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuardarApplication.class, args);
+	}
+}
---
+++ Guardar/src/main/java/com/accenture/guardar/Informacion.java
@@ -0,0 +1,5 @@
+package com.accenture.guardar;
+
+public class Informacion {
+
+}
