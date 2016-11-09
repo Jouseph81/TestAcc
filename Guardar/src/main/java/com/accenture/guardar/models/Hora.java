50584bb0154f7d27a39dfce8ca3195957eeba7c4
josep.marco.selma <josep.marco.selma@192.168.1.38>
2016-11-09 09:46:43 +0100

    BD Afegida

--- Guardar/src/main/java/com/accenture/guardar/models/Hora.java
+++ Guardar/src/main/java/com/accenture/guardar/models/Hora.java
@@ -1,10 +1,19 @@
-package com.accenture.guardar;
+package com.accenture.guardar.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.joda.time.LocalTime;
 
+@Entity
+@Table(name = "Horas")
 public class Hora {
 
+	@Id
 	private final long id;
+	@Column(nullable = false)
 	private final LocalTime hora;
 	
 	public Hora(long id, LocalTime hora) {
